62c8b9cf83d16b1caf3216773718f7f92efc8a57
Thanh Ngân <hieubdn@gmail.com>
2023-09-19 20:26:09 +0700

    lesson 6

--- Lesson6.py
+++ Lesson6.py
@@ -1,3 +1,19 @@
+# Câu hỏi: Chương trình này để làm gì?
+sum = 0
+for i in range(10): 
+	if i % 2 == 0:
+		sum = sum + i
+print(sum)
+# Trả lời: Tính tổng các số chẳng từ 0 đến 9
+
+# -----***-----
+# Câu hỏi: Vòng lặp này chạy bao nhiêu lần?
+n = 5
+for i in range (n+1):
+		print("Xin chào, tôi là MindX")
+# Trả lời: in ra 5 lần
+# **********************------***************************
+
 # Xuất ra màng hình dãy số thứ tự 1 -> 9
 for i in range (10):
     print(i)
